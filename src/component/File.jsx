14637d41cb972a4f4c534c1271720cabc31e68c5
zhubiao <1967349734@qq.com>
2020-09-10 01:05:04 +0800

    恢复antd样式

--- src/component/File.jsx
+++ src/component/File.jsx
@@ -1,8 +1,8 @@
 import React,{Component} from 'react';
 import { Upload, Button } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
 import './File.less';
-
+const {Dragger} = Upload;
 export default class File extends Component {
   render(){
     return (
@@ -10,6 +10,17 @@ export default class File extends Component {
         <Upload >
           <Button icon={<UploadOutlined />}>Click to Upload</Button>
         </Upload>
+
+        <Dragger>
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+              <p className="ant-upload-text">Click or drag file to this area to upload</p>
+              <p className="ant-upload-hint">
+                Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+                band files
+          </p>
+        </Dragger>
       </div>
     )
   }
