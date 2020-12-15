0b7c6e70b0ba45d70dbedc3921b79ebf028291a9
19A <1967349734@qq.com>
2020-12-16 00:50:28 +0800

    计数器redux的demo

--- src/index.js
+++ src/index.js
@@ -3,14 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import { createStore } from 'redux';
+const reducer = (state=0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':return state+1;
+    case 'DECREASE': return state-1;
+    default:return state;
+  }
+}
+const store = createStore(reducer);
+const Counter = ({value,onIncrement,onDecrease}) => {
+  return (<div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrease}>-</button>
+  </div>)
+}
+const render = () => {
+  ReactDOM.render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <Counter 
+    value = {store.getState()}
+    onIncrement = {()=> store.dispatch({type:'INCREMENT'})}
+    onDecrease = {()=> store.dispatch({type:'DECREASE'})}
+ >
+  </Counter>
+  ,
   document.getElementById('root')
 );
-
+}
+render();
+store.subscribe(render);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
